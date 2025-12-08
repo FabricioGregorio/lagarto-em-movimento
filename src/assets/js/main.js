a083e7df5b485e76691b2987a7d7133c1ae5e050
Fabrício Gregório <fabriciogregorio111@gmail.com>
2025-12-08 18:58:32 -0300

    adc de conteudo no index e mudanças visuais

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,29 @@
-import '../css/style.css' 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+/* src/assets/js/main.js */
+
+// Espera o HTML carregar completamente antes de rodar o script
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // 1. Seleciona os elementos
+    const clickableImages = document.querySelectorAll('.img-clickable');
+    const modalImage = document.getElementById('modalImage');
+  
+    // Verifica se os elementos existem para evitar erros
+    if (clickableImages.length > 0 && modalImage) {
+        
+        // 2. Adiciona o evento de clique em CADA imagem
+        clickableImages.forEach((img) => {
+            img.addEventListener('click', function() {
+                
+                // Teste no Console (Aperte F12 para ver se aparece)
+                console.log("Imagem clicada:", this.src);
+    
+                // 3. Pega o SRC da imagem clicada e joga na imagem do modal
+                modalImage.src = this.src;
+                modalImage.alt = this.alt;
+            });
+        });
+        
+    } else {
+        console.log("Nenhuma imagem clicável ou modal encontrado.");
+    }
+});
